f5f00eb62afb9f8efe40cd67060604d12d2bc52e
Kyle <florez.photo@gmail.com>
2021-03-08 18:25:14 -0600

    little psuedocoding

---
+++ sheduler.js
@@ -0,0 +1,12 @@
+// so calender based on 9-5 work day
+// iterated through the current hour colors that box red if true
+// past hours are grey
+// upcoming hours green
+// save icon
+// current day and time at top of the planner
+// saves user input into local storage
+// uhhhhh think thats it tbh...
+
+
+
+
