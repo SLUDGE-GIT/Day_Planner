5f566938702210479a680f4679d3ec90bfe8d88e
Kyle <florez.photo@gmail.com>
2021-03-10 07:14:12 -0600

    honestly, i got to the end of this assignment only to realize my save button didnt work and the colors wouldnt work so im gonna restart after I wake up (its 7am.. lol) uhhh yeah idk

--- scheduler.js
+++ scheduler.js
@@ -9,48 +9,81 @@
 
 window.onload = function (){
 
-    let today = new Date();
+    // let today = new Date();
     let date = moment().format('LL');
     $('#currentDay').html(date);
 
 }
 
-// // set up the schedule from 9-5
-let scheduleHours = ['9 AM','10 AM','11 AM','12 PM','1 PM','2 PM','3 PM','4 PM','5 PM'];
+// set up the schedule table from 9-5
+let scheduleHours = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
 let $container = $('.container');
 
+    $.each(scheduleHours, function (index, value){
+        let bgColor = getTime(value);
+        let $row = $('<div>').addClass('row');
+        let $col1 = $('<div>' + value + '</div>').addClass('col-sm-1 hour time');
+        let $col2 = $('<div></div>').addClass('col-sm-10 past decription ' + bgColor + ' time-' + value);
+        let $col3 = $('<button></button>').addClass('col-sm-1 saveBtn');
+        let textarea = $('<textarea></textarea>').addClass('col-sm-12 input');
+        let saveIcon = $('<i class = "fas fa-save" style = "margin: auto; padding: 10px;"></i>');
+
+            $row.append($col1);
+            $row.append($col2);
+            $col2.append(textarea);
+            $row.append($col3);
+            $col3.append(saveIcon);
+            $container.append($row);
+
+    })
+
+
+    $.each(scheduleHours, function (index, value){
+
+        $('.time-' + value + ' .input').val(localStorage.getItem(value))
+
+    })
 
 
 
+    function getTime(now){
+       
+        let date = new Date();
+        let hours = date.getHours();
+        // let mins = date.getMinutes();
+        
+        let am = 'AM';
+        let pm = 'PM'; 
+        let hoursAmPm;
+
+        //add am or pm to time
+        if(hours > 12){
+            hoursAmPm = pm;
+        } else{
+            hoursAmPm = am;
+        }
+
+        hours = hours % 12;
+        
+        // sanity check  -- if hours is true then do nothing - if false set hours to 12;
+        if(hours){
+            // hours = hours;
+        } else {
+            hours = 12;
+        }
+        
+        // we dont really need to check minutes - feel like that is pointless
+        //  if(mins < 10){
+        //     mins = '0' + mins;       
+        // } else {
+        //     mins = mins;
+        // }
+
+        
+        // color changer ---- maybe do all this in its own function  - for now i just want it all to work D:<
+        // im so stupid theres 'past' 'present' 'future' that has colors set for these already
+        return ''
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-// check to assign 'AM' or 'PM' to what tables are what colors
-// since we are doing 9am-5pm we make an if statement to loop through and it will be easier to say that 5pm = 18:00 
-
-// function colorChange(hours){}
-
-
-function amPm(){
-let am = ' AM';
-let pm = ' PM';
-
-if (hours > 12){
-hours = hours + pm
-} else {
- hours = hours + am
-
-}
-return hours;
-
-}
+    
